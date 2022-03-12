975a47a858b9598924f9c9abcf6077c44bf47879
nellynakhero <nelly.nakhero@gmail.com>
2022-03-12 07:37:41 -0800

    Write a program that prints all possible different combinations of two digits.
    Numbers must be separated by ,, followed by a space
    The two digits must be different

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
+ * 	in ascending order, separated by a comma followed by a space.
  * Return: Always 0.
  */
-int main(void)
-{
+
+int main(void){
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
@@ -18,7 +17,7 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-				continue;			
+				continue;
 
 			putchar(',');
 			putchar(' ');
@@ -26,6 +25,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 }

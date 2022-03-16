4daae562db33acfc96173aaad4bf6cc1cd9e4885
nellynakhero <nelly.nakhero@gmail.com>
2022-03-16 02:27:35 -0700

    Write a function that prints the n times table, starting with 0.
    Prototype: void print_times_table(int n);
    If n is greater than 15 or less than 0 the function should not print anything

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,37 +1,39 @@
 
 #include "main.h"
-/**                                                                                                                                                                                             
- * putformat - formatted characters to output                                                                                                                                                   
- * @n: number to format                                                                                                                                                                         
- * Return: nothing                                                                                                                                                                              
- *                                                                                                                                                                                              
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ *
  */
-void putformat(int n)                                                                                                                                                                           
-{                                                                                                                                                                                               
-	if (n <= 9)                                                                                                                                                                             
-	{                                                                                                                                                                                       
-		_putchar(',');                                                                                                                                                                  		_putchar(' ');                                                                                                                                                                  
-		_putchar(' ');                                                                                     
-		_putchar(' ');                                                                                                                                                                  		_putchar(n + '0');                                                                                                                                                              
-	}                                                                                                                                                                          
-	else if (n > 9 && n <= 99)
-	{                                                                                                                                                                                       
-		_putchar(',');                                                                                                                                                                  
-		_putchar(' ');                                                                                                                                                                  
-		_putchar(' ');                                                                                                                                                                  
-		_putchar(n / 10 + '0');                                                                                                                                                         
-		_putchar(n % 10 + '0');                                                                                                                                                         
-	}                                                                                                                                                                                       
-	else                                                                                                                                                                                    
-	{                                                                                                                                                                                       
-		_putchar(',');                                                                                                                                                                  
-		_putchar(' ');                                                                                                                                                                  
-		_putchar(n / 100 + '0');                                                                                                                                                        
-		_putchar(n / 10 % 10 + '0');                                                                                                                                                    
-		_putchar(n % 10 + '0');                                                                                                                                                         
-	}                                                                                                                                                                                       
-}
 
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
 /**
  *  print_times_table - prints times table for numbers from 0-14
  *  @n: An input integer value
